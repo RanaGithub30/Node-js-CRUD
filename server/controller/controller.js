29dfcce75d328f92abb2963b5a27874f330bb585
Rana Saha <ranasaha03edu@gmail.com>
2023-02-25 21:11:02 +0530

    CRUD Apis

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -34,16 +34,63 @@ exports.create = (req, res) => {
 
 // retrive and return all user/ return a single user
 exports.find = (req, res) => {
-    
+    userdb.find()
+    .then(user => {
+        res.send(user)
+    })  
+    .catch(err => {
+        res.status(500).send({
+           message: err.message || "Some error occured while Retriving User Information"
+        });
+    });  
 }
 
 // update a user by user id
 exports.update = (req, res) => {
-    
+    // validate request
+    if(!req.body){
+        res.status(400).send({ message: "Content Can not be empty!" });
+        return;
+    }
+
+    // get id from request parameter
+    const id = req.params.id;
+    userdb.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+    .then(data => {
+        if(!data){
+              res.status(404).send({ message: "Cann't update user.. Maybe, User not found"})   
+        }else{
+            res.send(data)
+        }
+    })
+
+    // if any error occur during update
+    .catch(err => {
+        res.status(500).send({
+           message: err.message || "Some error occured while Updating User Information"
+        });
+    });
 }
 
 // delete a user by user id
 exports.delete = (req, res) => {
-    
-}
+     const id = req.params.id;
+     
+     userdb.findByIdAndDelete(id)
+     .then(data => {
+        if(!data){
+            res.status(404).send({ message: "Cann't delete user.. Maybe, the Id is wrong"})   
+        }else{
+            res.send({
+                message: "User deleted Successsfully"
+            })
+        }
+     })
 
+     // if any error occur during delete
+    .catch(err => {
+        res.status(500).send({
+           message: err.message || "Some error occured while Deleting User Information"
+        });
+    });
+}
